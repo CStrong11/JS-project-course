6df235a9283dc54cdcf557fc4ecbd0cebb458c00
Cristian Morales <cristianmorales1198@gmail.com>
2023-05-06 07:57:22 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,52 @@
+function computerPlay() {
+  const choices = ["rock", "paper", "scissors"];
+  const randomIndex = Math.floor(Math.random() * choices.length);
+  return choices[randomIndex];
+}
+
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection === computerSelection) {
+    return "It's a tie!";
+  } else if (
+    (playerSelection === "rock" && computerSelection === "scissors") ||
+    (playerSelection === "paper" && computerSelection === "rock") ||
+    (playerSelection === "scissors" && computerSelection === "paper")
+  ) {
+    return `You Win! ${playerSelection} beats ${computerSelection}.`;
+  } else {
+    return `You Lose! ${computerSelection} beats ${playerSelection}.`;
+  }
+}
+
+const playerSelection = "rock";
+const computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection));
+
+function game() {
+  let playerScore = 0;
+  let computerScore = 0;
+  for (let i = 1; i <= 5; i++) {
+    const playerSelection = prompt(
+      `Round ${i}: Enter your choice (Rock/Paper/Scissors):`
+    ).toLowerCase();
+    const computerSelection = computerPlay().toLowerCase();
+    const result = playRound(playerSelection, computerSelection);
+    console.log(result);
+    if (result.startsWith("You Win!")) {
+      playerScore++;
+    } else if (result.startsWith("You Lose!")) {
+      computerScore++;
+    }
+  }
+  if (playerScore > computerScore) {
+    console.log(
+      `You won the game! Final score: ${playerScore} - ${computerScore}`
+    );
+  } else if (playerScore < computerScore) {
+    console.log(
+      `You lost the game! Final score: ${playerScore} - ${computerScore}`
+    );
+  } else {
+    console.log(`It's a tie! Final score: ${playerScore} - ${computerScore}`);
+  }
+}
