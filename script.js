bd79c718b243145005889bf0c858976b220897e6
Cristian Morales <cristianmorales1198@gmail.com>
2023-05-07 19:02:38 -0500

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,13 @@ function computerPlay() {
 }
 
 function playRound(playerSelection, computerSelection) {
+  const options = ["rock", "paper", "scissors"];
+  if (!playerSelection || playerSelection.toLowerCase().trim() === "") {
+    return "Invalid selection. Please choose Rock, Paper, or Scissors.";
+  }
+  if (!options.includes(playerSelection)) {
+    return `Invalid selection. "${playerSelection}" is not a valid option. Please choose Rock, Paper, or Scissors.`;
+  }
   if (playerSelection === computerSelection) {
     return "It's a tie!";
   } else if (
@@ -19,14 +26,26 @@ function playRound(playerSelection, computerSelection) {
 }
 
 function game() {
+  alert(
+    `Welcome to the game of rock-scissors-paper, you will play 5 round against the computer. type your choice and let have fun!`
+  );
   let playerScore = 0;
   let computerScore = 0;
   for (let i = 1; i <= 5; i++) {
     const playerSelection = prompt(
       `Round ${i}: Enter your choice (Rock, Paper, Scissors):`
-    )
-      .toLowerCase()
-      .trim();
+    );
+    if (playerSelection === null) {
+      const shouldExit = confirm("Are you sure you want to exit the game?");
+
+      if (shouldExit) {
+        alert("Thanks for playing!");
+        return;
+      } else {
+        i--;
+        continue;
+      }
+    }
     const computerSelection = computerPlay().toLowerCase();
     const result = playRound(playerSelection, computerSelection);
     console.log(result);
@@ -34,6 +53,10 @@ function game() {
       playerScore++;
     } else if (result.startsWith("You Lose!")) {
       computerScore++;
+    } else if (result.startsWith("It's a tie!")) {
+      continue;
+    } else {
+      i--;
     }
   }
   if (playerScore > computerScore) {
